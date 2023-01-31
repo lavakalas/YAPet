5fdec271604c3a2a398cd1b556abedf32eeff4ce
lavakalas <lavakalas@mail.ru>
2023-01-31 19:45:10 +0200

    More of base code here...

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-# Init .py file
+import requests
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+
+class MainWindow(QWidget):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.resize(800, 600)
+        self.setWindowTitle('YAPet')
+        self.show()
+
+    def refresh(self):
+        pass
+
+
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    sys.exit(app.exec_())
