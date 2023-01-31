f50d313dff29f941226ac07b8b8347dbf299a502
lavakalas <lavakalas@mail.ru>
2023-01-31 20:20:03 +0200

    Базовый запрос к апи ЯКарт, отображение через Qt

--- main.py
+++ main.py
@@ -7,14 +7,37 @@ from PyQt5.QtWidgets import *
 
 class MainWindow(QWidget):
     def __init__(self, parent=None):
+        
+        self.map_ll = [37.977751, 55.757718]
+        self.map_l = "map"
+        self.map_zoom = 5
+
+        
+        
         super(MainWindow, self).__init__(parent)
-        self.resize(800, 600)
+        self.loadUI()
+        self.resize(600, 450)
         self.setWindowTitle('YAPet')
         self.show()
+        self.refresh()
 
     def refresh(self):
-        pass
-
+        params = {
+            "ll": ','.join(map(str, self.map_ll)),
+            "l": self.map_l,
+            'z': self.map_zoom
+        }
+        response = requests.get('https://static-maps.yandex.ru/1.x/', params=params)
+        with open('pic.png', 'wb') as pic:
+            pic.write(response.content)
+        
+        pixmap = QPixmap()
+        pixmap.load('pic.png')
+        self.label.setPixmap(pixmap)
+    
+    def loadUI(self):
+        self.label = QLabel(self)
+        self.label.resize(600,450)
 
 
 
